0d57fde0b73eee5d74ad631d48cc945daac91dc0
mariakeeton <maria.keeton@gmail.com>
2015-01-21 06:17:52 -0500

    Added functionality to button - adding items to list (Including modified files/folders)

--- js/listapp.js
+++ js/listapp.js
@@ -1,2 +1,17 @@
 $(document).ready(function() {
+	//Check to see if an item is entered in textbox
+	$('.add-item-btn').click(function() {
+	//If it isn't, alert user
+		if($('.item').val().trim().length == 0){
+			$('.error').show();
+			$('.add-item-btn').val();
+		} //If it is, add item to list
+		else {
+			$('.list').append('<li class="unchecked"><div class="checkbox"></div>' + $(".item").val() + '</li>');
+			$('.item').val("");
+			$('.error').hide();
+		}
+
+	})
+
 });
