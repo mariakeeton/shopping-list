78688cc94edf17cb288fb5cc19c0af964a2a3f62
mariakeeton <maria.keeton@gmail.com>
2015-01-24 14:15:01 -0500

    Added sort feature and functioning delete button

--- js/listapp.js
+++ js/listapp.js
@@ -13,10 +13,20 @@ function add_item() {
 			$('.add-item-btn').val();
 		} //If it is, add item to list
 		else {
-			$('.list').append('<li class="unchecked"><div class="checkbox"></div>' + $(".item").val() + '</li>');
+			$('.list').append('<li class="unchecked"><div class="checkbox"></div>' + $(".item").val().toLowerCase() + '</li>');
 			$('.item').val("");
 			$('.error').hide();
 		}
+		var listItems = $('.list li').get();
+		listItems.sort(function(a,b){
+			var x = $(a).text();
+			var y = $(b).text();
+
+			return x < y ? -1 : x > y ? 1 : 0;
+	});
+		$.each(listItems, function(index, li) {
+		$('.list').append(li);
+		})
 
 	})
 		
@@ -28,9 +38,21 @@ function check_item() {
 }
 function remove_item() {
 	$('ul.list').on('mouseenter', 'li', function() {
-			$(this).append('<button class="remove-item-btn">Remove</button>');
+			$(this).append('<button class="remove-item-btn">Delete</button>');
 	})
 	$('ul.list').on('mouseleave', 'li', function() {
 			$('.remove-item-btn').remove();
 	})
+	$('ul.list').on('click', '.remove-item-btn', function() {
+			$(this).parent().remove();
+	})
 }
+/*function sort_items() {
+	var listItems = $('ul.list li').get();
+	console.log(listItems.text());
+	listItems.sort();
+	$.each(listItems, function(i, li) {
+		$('ul.list').append(li);
+	})
+
+}*/
