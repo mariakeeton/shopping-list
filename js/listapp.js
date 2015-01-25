14fccde60b1c925edd46c4bcd641667b8f4388de
mariakeeton <maria.keeton@gmail.com>
2015-01-25 12:55:49 -0500

    Removed code from inside functions - delete function wrappers

--- js/listapp.js
+++ js/listapp.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
-	add_item();
-	check_item();
-	remove_item();
-	
-});
-//Check to see if an item is entered in textbox
-function add_item() {
+	//Check to see if an item is entered in textbox
 	$('.add-item-btn').click(function() {
 	//If it isn't, alert user
 		if($('.item').val().trim().length == 0){
@@ -17,26 +11,26 @@ function add_item() {
 			$('.item').val("");
 			$('.error').hide();
 		}
+		
+		//Sort Items
 		var listItems = $('.list li').get();
 		listItems.sort(function(a,b){
 			var x = $(a).text();
 			var y = $(b).text();
 
 			return x < y ? -1 : x > y ? 1 : 0;
-	});
+		});
 		$.each(listItems, function(index, li) {
 		$('.list').append(li);
 		})
+	});
 
-	})
-		
-}
-function check_item() {
+	//Check or uncheck item
 	$('ul.list').on('click', '.checkbox', function() {
 			$(this).parent().toggleClass('unchecked checked');
 	})
-}
-function remove_item() {
+
+	//Delete Item from list
 	$('ul.list').on('mouseenter', 'li', function() {
 			$(this).append('<button class="remove-item-btn">Delete</button>');
 	})
@@ -46,13 +40,5 @@ function remove_item() {
 	$('ul.list').on('click', '.remove-item-btn', function() {
 			$(this).parent().remove();
 	})
-}
-/*function sort_items() {
-	var listItems = $('ul.list li').get();
-	console.log(listItems.text());
-	listItems.sort();
-	$.each(listItems, function(i, li) {
-		$('ul.list').append(li);
-	})
 
-}*/
+});
